1d795a2b269b6b331ef2f0d843254bf2c87437d1
Naud Ghebre <nghebre3@gatech.edu>
2019-04-20 18:09:48 -0400

    chess ai tree search

--- my_agent.py
+++ my_agent.py
@@ -12,6 +12,7 @@ Source:         Adapted from recon-chess (https://pypi.org/project/reconchess/)
 import random
 import chess
 import numpy as np
+import time
 from player import Player
 
 
@@ -40,11 +41,15 @@ class Dior(Player):
         # TODO: implement this method
         self.board = board
         self.color = color
+        self.opponent = chess.BLACK if color == chess.WHITE else chess.WHITE
+        self.DEPTH = 2
         self.sensestaken = []
         self.trackedKing = (False, None)
         self.move_counter = 5
         self.moves_to_kings = 0
-        self.interest_squares = [34,35,60]
+        self.global_counter = 0
+        self.start = 0
+        self.interest_squares = []
         
     def handle_opponent_move_result(self, captured_piece, captured_square):
         """
@@ -56,6 +61,41 @@ class Dior(Player):
         self.my_piece_captured_square = captured_square
         if captured_piece:
             self.board.remove_piece_at(captured_square)
+
+    # def choose_sense(self, possible_sense, possible_moves, seconds_left):
+    #     """
+    #     This function is called to choose a square to perform a sense on.
+    #
+    #     :param possible_sense: List(chess.SQUARES) -- list of squares to sense around
+    #     :param possible_moves: List(chess.Moves) -- list of acceptable moves based on current board
+    #     :param seconds_left: float -- seconds left in the game
+    #
+    #     :return: chess.SQUARE -- the center of 3x3 section of the board you want to sense
+    #     :example: choice = chess.A1
+    #     """
+    #     # TODO: update this method
+    #     if (self.move_counter == 0 and self.color == chess.WHITE):
+    #         return 52
+    #     elif(self.move_counter == 0 and self.color == chess.BLACK):
+    #         return 12
+    #
+    #     new_senses = set(possible_sense) - set([0,1,2,3,4,5,6,7,8,15,16,18,19,20,21,23,24,26,29,31,32,34,37,39,40,42,43,44,45,47,48,55,56,57,58,59,60,61,62,63])
+    #     new_senses_list = list(new_senses)
+    #     print('possible senses', list(new_senses))
+    #     print('possible moves', possible_moves)
+    #     choosing = True
+    #     while choosing:
+    #
+    #         choice = random.choice(new_senses_list)
+    #         if choice not in self.sensestaken:
+    #             self.sensestaken += [choice]
+    #             # print(self.sensestaken)
+    #             choosing = False
+    #             print(choice)
+    #             if len(self.sensestaken) == 24:
+    #                 self.sensestaken = []
+    #             # print(self.sensestaken)
+    #     return choice
 
     def choose_sense(self, possible_sense, possible_moves, seconds_left):
         """
@@ -71,37 +111,58 @@ class Dior(Player):
         # TODO: update this method
         if (self.move_counter == 0 and self.color == chess.WHITE):
             return 52
-        elif(self.move_counter == 0 and self.color == chess.BLACK):
+        elif (self.move_counter == 0 and self.color == chess.BLACK):
             return 12
 
-        new_senses = set(possible_sense) - set([0,1,2,3,4,5,6,7,8,15,16,18,19,20,21,23,24,26,29,31,32,34,37,39,40,42,43,44,45,47,48,55,56,57,58,59,60,61,62,63])
+        new_senses = set(possible_sense) - {0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 18, 19, 20, 21, 23, 24, 26, 29, 31, 32,
+                                            34, 37, 39, 40, 42, 43, 44, 45, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63}
         new_senses_list = list(new_senses)
+        best_white_senses = list(new_senses - {9, 10, 11, 12, 13, 14, 27, 28})
+        best_black_senses = list(new_senses - {49, 50, 51, 52, 53, 54, 36, 37})
+
         print('possible senses', list(new_senses))
         print('possible moves', possible_moves)
         choosing = True
         while choosing:
-
-            choice = random.choice(new_senses_list)
-            if choice not in self.sensestaken:
-                self.sensestaken += [choice]
-                # print(self.sensestaken)
-                choosing = False
-                print(choice)
-                if len(self.sensestaken) == 24:
-                    self.sensestaken = []
-                # print(self.sensestaken)
+            if self.move_counter < 30:
+                if self.color == chess.BLACK:
+                    choice = random.choice(best_black_senses)
+                    if choice not in self.sensestaken:
+                        self.sensestaken += [choice]
+                        # print(self.sensestaken)
+                        choosing = False
+                        print(choice)
+                        if len(self.sensestaken) == 8:
+                            self.sensestaken = []
+                            # print(self.sensestaken)
+                elif self.color == chess.WHITE:
+                    choice = random.choice(best_white_senses)
+                    if choice not in self.sensestaken:
+                        self.sensestaken += [choice]
+                        # print(self.sensestaken)
+                        choosing = False
+                        print(choice)
+                        if len(self.sensestaken) == 8:
+                            self.sensestaken = []
+                            # print(self.sensestaken)
+            else:
+                choice = random.choice(new_senses_list)
+                if choice not in self.sensestaken:
+                    self.sensestaken += [choice]
+                    # print(self.sensestaken)
+                    choosing = False
+                    print(choice)
+                    if len(self.sensestaken) == 24:
+                        self.sensestaken = []
         return choice
 
-
-
-        
     def handle_sense_result(self, sense_result):
         """
         This is a function called after your picked your 3x3 square to sense and gives you the chance to update your
         board.
 
         :param sense_result: A list of tuples, where each tuple contains a :class:`Square` in the sense, and if there
-                             was a piece on the square, then the corresponding :class:`chess.Piece`, otherwise `None`.
+                             was a piece on the square, then the corresponding :class:`chess.Piece`, otherwise None.
         :example:
         [
             (A8, Piece(ROOK, BLACK)), (B8, Piece(KNIGHT, BLACK)), (C8, Piece(BISHOP, BLACK)),
@@ -112,15 +173,53 @@ class Dior(Player):
         # TODO: implement this method
         # Hint: until this method is implemented, any senses you make will be lost.
         kingRecentlyTracked = False
+        self.interest_squares = []
         for square, piece in sense_result:
             if piece != None:
-                if piece.symbol() == 'k':
-                    self.trackedKing = (True, square)
+                print("PIECE SENSED", piece)
+                print("SQUARE", square)
+                if self.color == chess.WHITE:
+                    if piece.symbol() == 'k' or piece.symbol() == 'q' or piece.symbol() == 'r':
+                        self.interest_squares += [square]
                     recentlyTracked = True
+                elif self.color == chess.BLACK:
+                    if piece.symbol() == 'K' or piece.symbol() == 'Q' or piece.symbol() == 'R':
+                        self.interest_squares += [square]
+
             self.board.set_piece_at(square, piece)
 
         if kingRecentlyTracked == False:
             self.trackedKing = (False, self.trackedKing[1])
+
+        if len(self.interest_squares) == 0:
+            self.interest_squares = [35, 36, 27, 28]
+
+    # def handle_sense_result(self, sense_result):
+    #     """
+    #     This is a function called after your picked your 3x3 square to sense and gives you the chance to update your
+    #     board.
+    #
+    #     :param sense_result: A list of tuples, where each tuple contains a :class:`Square` in the sense, and if there
+    #                          was a piece on the square, then the corresponding :class:`chess.Piece`, otherwise `None`.
+    #     :example:
+    #     [
+    #         (A8, Piece(ROOK, BLACK)), (B8, Piece(KNIGHT, BLACK)), (C8, Piece(BISHOP, BLACK)),
+    #         (A7, Piece(PAWN, BLACK)), (B7, Piece(PAWN, BLACK)), (C7, Piece(PAWN, BLACK)),
+    #         (A6, None), (B6, None), (C8, None)
+    #     ]
+    #     """
+    #     # TODO: implement this method
+    #     # Hint: until this method is implemented, any senses you make will be lost.
+    #     kingRecentlyTracked = False
+    #     for square, piece in sense_result:
+    #         if piece != None:
+    #             if piece.symbol() == 'k':
+    #                 self.trackedKing = (True, square)
+    #                 recentlyTracked = True
+    #         self.board.set_piece_at(square, piece)
+    #
+    #     if kingRecentlyTracked == False:
+    #         self.trackedKing = (False, self.trackedKing[1])
 
     def choose_move(self, possible_moves, seconds_left):
         """
@@ -196,63 +295,56 @@ class Dior(Player):
     #
     #     board.pop()
     def test_algo(self, possible_moves, seconds_left):
-        locations = []
-        print(self.board.legal_moves)
-        # print(possible_moves)
-        our_pieces_moves = []
 
-        for choice in possible_moves:
-            # print(choice.from_square)
-            # print(our_pieces)
-            if choice.from_square not in locations:
-                locations.append(choice.from_square)
-                # print(self.board.piece_at(choice.from_square))
-                # print(choice)
-                # print(self.board.piece_at(choice.from_square).color)
-                our_pieces_moves.append((self.board.piece_at(choice.from_square),self.piece_ranks[self.board.piece_at(choice.from_square).symbol().lower()], choice))
+        boards_list = [self.board.copy() for i in range(len(possible_moves))]
+        depth_count = []
+        for move_index in range(len(possible_moves)):
+            our_pieces_moves = []
+
+            choice = possible_moves[move_index]
+
+            # Make each move on separate board state
+            boards_list[move_index].set_piece_at(choice.to_square,
+                                                 boards_list[move_index].piece_at(choice.from_square))
+            boards_list[move_index].remove_piece_at(choice.from_square)
+
+            for i in boards_list[move_index].legal_moves:
+                our_pieces_moves.append((boards_list[move_index].piece_at(choice.from_square),
+                                         self.piece_ranks[boards_list[move_index].piece_at(i.from_square).symbol().lower()],
+                                         i))
+            depth_count.append(self.find_best_move(boards_list[move_index], our_pieces_moves, 0))
+
+        try:
+            if len(depth_count) == 0:
+                return random.choice(possible_moves)
             else:
-                # print(self.board.piece_at(choice.from_square))
-                # print(choice)
-                our_pieces_moves.append((self.board.piece_at(choice.from_square),self.piece_ranks[self.board.piece_at(choice.from_square).symbol().lower()], choice))
-
-        # print(locations)
-        # print(len(our_pieces))
-        for i in our_pieces_moves:
-            print(i)
+                return possible_moves[np.argmin(np.array(depth_count))]
+        except:
+            print(depth_count)
 
 
-        print("SORTED MOVES LIST")
-        for i in our_pieces_moves:
-            print(i)
+    def find_best_move(self, board, our_pieces_moves, moves_to_king):
+        self.global_counter += 1
+        print(self.global_counter)
 
+        if board.king(self.opponent) == None:
+            return print("MOVES TO KING", moves_to_king)
 
-            # print(i.piece_type, i.color, i.symbol())
-
-        moves_to_king = 0
-
-        our_pieces_moves.sort(key=lambda tup: tup[1], reverse=True)
-        the_move_index = self.find_best_move(our_pieces_moves, possible_moves, moves_to_king)
-        # print(the_move_index, len(our_pieces_moves))
-        # print(our_pieces_moves[the_move_index])
-        if the_move_index < len(our_pieces_moves):
-            return our_pieces_moves[the_move_index][2]
-        else:
-            return our_pieces_moves[the_move_index % len(our_pieces_moves)][2]
-
-
-
-
-
-    def find_best_move(self,our_pieces_moves, possible_moves, moves_to_king):
         moves_to_king += 1
-        our_pieces_moves.sort(key=lambda tup: tup[1], reverse=True)
-        boards_list = [self.board.copy() for i in range(len(our_pieces_moves))]
-        best_move = -1
-
-        while moves_to_king <= 6:
+        if moves_to_king > self.DEPTH:
+            return 10
+        else:
+            boards_list = [board.copy() for i in range(len(our_pieces_moves))]
+            depth_count = []
             for move_index in range(len(our_pieces_moves)):
-                # print('here')
-                new_moves_list = []
+                # print('here1')
+
+                # Make each move on separate board state
+
+                if our_pieces_moves[move_index][2].to_square in self.interest_squares:
+                    depth_count.append(moves_to_king)
+                    continue
+
                 boards_list[move_index].set_piece_at(our_pieces_moves[move_index][2].to_square, our_pieces_moves[move_index][0])
                 boards_list[move_index].remove_piece_at(our_pieces_moves[move_index][2].from_square)
                 # for poss_next_move_index in range(len(our_pieces_moves)):
@@ -260,23 +352,32 @@ class Dior(Player):
                 #             #         new_moves_list += [our_pieces_moves[poss_next_move_index]]
                 #             #
                 #             # print(new_moves_list)
+
+                new_moves_list = []
                 for i in boards_list[move_index].legal_moves:
-                    # print('here')
-                    new_moves_list.append((boards_list[move_index].piece_at(i.from_square),self.piece_ranks[boards_list[move_index].piece_at(i.from_square).symbol().lower()], i,))
+                    # print('here2')
+                    new_move = (boards_list[move_index].piece_at(i.from_square),
+                                self.piece_ranks[boards_list[move_index].piece_at(i.from_square).symbol().lower()], i,)
+                    new_moves_list.append(new_move)
 
+                    # # if the king is found or destination is in interest squares then return this move index
+                    # if new_move[2].to_square in self.interest_squares:
+                    #     return moves_to_king + 1
+                    # else:
 
-                # if the king is found then return this move index
-                for i in new_moves_list:
-                    print(i[2].to_square)
-                    if i[2].to_square in self.interest_squares:
-                        print('here')
-                        return move_index
-                if moves_to_king > 6:
-                    return random.randint(0,len(our_pieces_moves))
-                else:
-                    moves_to_king += 1
-                    return self.find_best_move(new_moves_list, possible_moves, moves_to_king)
-        return random.randint(0,len(our_pieces_moves))
+                # for i in new_moves_list:
+                #     print(i[2].to_square)
+                #     if i[2].to_square in self.interest_squares:
+                #         print('here3')
+                #         return move_index
+                count = self.find_best_move(boards_list[move_index], new_moves_list, moves_to_king)
+                if count != None:
+                    depth_count.append( count )
+
+            if depth_count == None or len(depth_count) == 0:
+                return 10
+            else:
+                return np.min(depth_count)
 
 
 
